8ec1ad916f1422ecb10396e8c6a51ef8378ce35f
Adenike23 <adeofeadenike@gmail.com>
2023-10-03 20:58:42 +0100

    media query updated

--- js/main.js
+++ js/main.js
@@ -2,16 +2,24 @@ const identity = document.querySelector('.identity');
 const advice = document.querySelector('.advice');
 const button = document.querySelector('button')
 async function getAdvice() {
+    
     const response = await fetch('https://api.adviceslip.com/advice', {
         method: 'GET'
-       
     })
-    console.log(response);
+   console.log(response);
     const data = await response.json();
-    console.log(data);
     identity.textContent = data.slip.id;
     advice.textContent = data.slip.advice;
 }
-getAdvice()
+
+// function getAdvice(){
+//     fetch('https://api.adviceslip.com/advice')
+//     .then(response => response.json())
+//     .then(data => {
+//         if(response.o)
+//     })
+//     .catch(err => console.log(err))
+// }
+// getAdvice()
 
 button.addEventListener('click', () => getAdvice())
